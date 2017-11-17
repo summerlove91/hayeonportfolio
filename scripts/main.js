8712dfc0c9c50269a11a22d98bb6cf43badc2426
summerlove91 <gkdus3040@gmail.com>
2017-11-16 16:44:24 -0800

    compressed large pic files

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
-
-$(window).load(function() {
-		// Animate loader off screen
-    $(".se-pre-con").fadeOut("slow");
-//    callback to delete thing with class .se-pre-con
-});
+//
+//$(window).load(function() {
+//		// Animate loader off screen
+//    $(".se-pre-con").fadeOut("slow");
+////    callback to delete thing with class .se-pre-con
+//});
