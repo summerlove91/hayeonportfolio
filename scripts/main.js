3504a81a870f69b72b8200fb4842920c0dd72fc9
summerlove91 <gkdus3040@gmail.com>
2018-03-16 01:08:34 -0700

    added more styles

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,17 @@
 //    $(".se-pre-con").fadeOut("slow");
 ////    callback to delete thing with class .se-pre-con
 //});
+
+//window.onload = function(){
+    $(document).ready(function(){
+
+        $('#mouseoverReveal').css('opacity', 0);
+
+        $('#mouseoverReveal').waypoint(function()
+            {
+            $('#mouseoverReveal').addClass('fadeInLeft');
+            $('#mouseoverReveal').css('opacity',1);},
+            {offset:'50%'});
+     }); 
+//}
+
