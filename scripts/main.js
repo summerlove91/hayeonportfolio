8d6bb171c4e029617c12ccf099c4905cf06ac509
summerlove91 <gkdus3040@gmail.com>
2018-07-09 02:06:53 -0700

    changed about me design

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,23 @@
-window.load = function(){
-    var projectTitle = document.getElementsByClassName('project-mouseover');
-    
-    projectTitle.addEventListener("mouseover", function(){
-        projectTitle.style.opacity = "0.8";
-    })
-    
-}
+//window.load = function(){
+//    var projectTitle = document.getElementsByClassName('project-mouseover');
+//    
+//    projectTitle.addEventListener("mouseover", function(){
+//        projectTitle.style.opacity = "0.8";
+//    })
+//    
+//};
+
+
+$(document).ready(function() {      
+    $(document).scroll(function() {
+        var y = $(this).scrollTop();
+        if (y > 880){
+            $('.navbar').css("background-color","rgba(255,255,255,0.5)");
+//            $('.my-style-intro').fadeIn("3500");
+            $('.my-style-all p').fadeIn("3500");
+         } else {
+          $('.my-style-all p').fadeOut("slow");
+//          $('.my-style-intro').fadeOut("slow");
+         }
+     });
+});
