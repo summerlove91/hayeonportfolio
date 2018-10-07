7cb883ee992e682050a9e1456ecf4ec2ba22cc14
summerlove91 <gkdus3040@gmail.com>
2018-10-06 22:18:19 -0700

    webpage mobile friendly

--- scripts/main.js
+++ scripts/main.js
@@ -11,13 +11,12 @@
 $(document).ready(function() {      
     $(document).scroll(function() {
         var y = $(this).scrollTop();
-        if (y > 880){
+        var x = $('.my-style-intro').scrollTop();
+        if (y > 890){
             $('.navbar').css("background-color","rgba(255,255,255,0.5)");
-//            $('.my-style-intro').fadeIn("3500");
             $('.my-style-all p').fadeIn("3500");
          } else {
           $('.my-style-all p').fadeOut("slow");
-//          $('.my-style-intro').fadeOut("slow");
          }
      });
 });
