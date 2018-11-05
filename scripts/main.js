b8f2509f91986d2c46b07516d7d023e67c9de78f
summerlove91 <gkdus3040@gmail.com>
2018-11-04 17:49:31 -0800

    updated css to be better mobile friendly

--- scripts/main.js
+++ scripts/main.js
@@ -7,16 +7,16 @@
 //    
 //};
 
-
-$(document).ready(function() {      
-    $(document).scroll(function() {
-        var y = $(this).scrollTop();
-        var x = $('.my-style-intro').scrollTop();
-        if (y > 890){
-            $('.navbar').css("background-color","rgba(255,255,255,0.5)");
-            $('.my-style-all p').fadeIn("3500");
-         } else {
-          $('.my-style-all p').fadeOut("slow");
-         }
-     });
-});
+//
+//$(document).ready(function() {      
+//    $(document).scroll(function() {
+//        var y = $(this).scrollTop();
+//        var x = $('.my-style-intro').scrollTop();
+//        if (y > 890){
+//            $('.navbar').css("background-color","rgba(255,255,255,0.5)");
+//            $('.my-style-all p').fadeIn("3500");
+//         } else {
+//          $('.my-style-all p').fadeOut("slow");
+//         }
+//     });
+//});
